eefd8e4b57e82c7702354a7ed8658b70739c28ea
phuctran <paul.tran@smarteevina.com>
2024-12-11 18:45:52 +0700

    - config cors

--- src/main/java/com/p5k/bacao/http/core/security/WebConfig.java
+++ src/main/java/com/p5k/bacao/http/core/security/WebConfig.java
@@ -1,6 +1,7 @@
 package com.p5k.bacao.http.core.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,8 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
+        registry
+                .addResourceHandler("/uploads/**","/**")
                 .addResourceLocations("file:uploads/");
     }
 }
